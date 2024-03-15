9ff33ec7b8a85fa86e3611b951deef25fa786090
Chu Hoang Viet <hgviet2004@gmail.com>
2024-03-16 01:44:08 +0700

    update prac 3

--- 3.student.mark.oop.py
+++ 3.student.mark.oop.py
@@ -9,13 +9,14 @@ class Student:
         self.__name = name
         self.__DoB = DoB
         self.__marks = {}
+        self.__gpa = None
         
     def set_mark(self,course,mark):
         # Use math module to round-down student scores to 1-digit decimal upon input, floor()
         if course in self.__marks:
-            self.__marks[course].append(math.floor(mark*10)/10)
+            self.__marks[course] = np.append(self.__marks[course],math.floor(mark*10)/10)
         else:
-            self.__marks[course]=[math.floor(mark*10)/10]
+            self.__marks[course]= np.array([math.floor(mark*10)/10])
         
     def get_id(self):
         return self.__id
@@ -26,13 +27,35 @@ class Student:
     def get_marks(self):
         return self.__marks
     
+    def get_gpa(self):
+        return self.__gpa
+    
+    def cal_gpa(self,courses):
+        gpa = 0
+        total_credits = 0
+        for in_course, marks in self.__marks.items():
+            for course in courses:
+                if in_course == course.get_name():
+                    credits = course.get_credits()
+                    total_credits += credits
+            gpa += float(np.mean(self.__marks[in_course])) * credits
+        gpa /= total_credits
+        self.__gpa = gpa
+            
+    def __gt__(self,other):
+        return self.__gpa > other.get_gpa()
+    
     def __str__(self):
         return f"{self.__id:<15}\t{self.__name:<30}\t{self.__DoB}"
                
 class Course:
-    def __init__(self,id,name):
+    def __init__(self,id,name,credits):
         self.__id = id
         self.__name = name
+        self.__credits = credits
+        
+    def get_credits(self):
+        return self.__credits
         
     def get_name(self):
         return self.__name
@@ -71,9 +94,10 @@ def set_course_in4(courses,n):
         print(f"Course {i+1}")
         id = input("course's id: ")
         name = input("course's name: ")
-        courses.append(Course(id,name))
+        credits = int(input("course's credits: "))
+        courses.append(Course(id,name,credits))
     
-def get_marks_for_each_course(students, courses):
+def set_marks_for_each_course(students, courses):
     if len(courses)==0:
         print("No course!")
     else:
@@ -117,26 +141,23 @@ def show_marks(students,courses):
                 marks = student.get_marks()[selected_course.get_name()]
                 print(f"{student.get_id():<15}\t{student.get_name():<30}: ",end='')
                 for i in range(0,len(marks)):
-                    print(f"{marks[i]} ",end = '')
+                    print(f"{marks[i]}", end=" ")
+                print("")
         
 def show_GPA(students,courses):
-    if len(courses)==0:
-        print("No course!")
+    if len(students)==0:
+        print("No student!")
     else:
-        print(f"Course list")
-        for i, course in enumerate(courses,start=1):
-            print(f"{i}. {course.get_name()}")
-        selected_course_index = to_int(input("Enter course number: ")) - 1
-        selected_course = courses[selected_course_index]
-        print(f"Showing GPA for students in the course '{selected_course.get_name()}':")
-        for student in students:
-            if selected_course.get_name() in student.get_marks():
-                marks = student.get_marks()[selected_course.get_name()]
-                gpa = np.array(marks).mean()
-                print(' '*48 + "GPA")
-                print(f"{student.get_id():<15}\t{student.get_name():<30}: {math.floor(gpa*10)/10}")
-
-def run():
+        for i in range(len(students)):
+            students[i].cal_gpa(courses)
+        for i in range(len(students)-1):
+            for j in range(0,len(students)-1-i):
+                if students[j+1] > students[j]:
+                    students[j], students[j+1] = students[j+1], students[j]
+        for i in range(len(students)):
+            print(f"{students[i].get_id():<15}{students[i].get_name():<30}{students[i].get_gpa():.2f}")
+            
+if __name__=="__main__":
     students = []
     courses = []
     student_nums = 0
@@ -174,7 +195,7 @@ Choose option:
             else:
                 set_course_in4(courses,course_nums)
         elif option == 5:
-            get_marks_for_each_course(students, courses)
+            set_marks_for_each_course(students, courses)
         elif option == 6:
             if student_nums == 0:
                 print(f"No student!")
@@ -191,6 +212,3 @@ Choose option:
             break
         else:
             print(f"Invalid. Please try again!")
-
-
-run()
